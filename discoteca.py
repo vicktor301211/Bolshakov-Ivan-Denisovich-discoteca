6e12d9c42a51976744cd565408c21eb39879e4f1
bolshakov863 <ivan.bolshakov113@gmail.com>
2025-03-01 15:48:22 +0300

    add new corrects

--- discoteca.py
+++ discoteca.py
@@ -1,31 +1,30 @@
-import pygame
 import random
+
+from pygame.draw import circle
+
 from all_colors import *
+import pygame
 pygame.init()
 
 #pygame.mixer.init()
 #pygame.mixer.music.load('music.mp3')
 #pygame.mixer.music.play(-1)
 
-size = (0, 0)
+size = (1280, 740)
 pygame.display.set_caption('Домашняя дискотека')
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+screen.fill((255, 255, 255))
 BACKGROUND = (255, 255, 255)
-COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (0, 0, 0)]
-running = True
+x = 0
+y = 0
+color = COLORS
 timer = 0
+pygame.display.flip()
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    for i in range(10):
-        x = random.randint(0, 0)
-        y = random.randint(0, 0)
-        radius = random.randint(10, 100)
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        pygame.draw.circle(screen, color, (x, y), radius)
-
 
     color_index = random.randint(0, 7)
     if color_index == 7:
@@ -33,10 +32,16 @@ while running:
     else:
         BACKGROUND = COLORS[color_index]
 
-
+    for i in range(10):
+        x = random.randint(0, 1280)
+        y = random.randint(0, 740)
+        radius = random.randint(10, 100)
+        color = COLORS
+        pygame.draw.circle(screen, random.choice(color), (x, y), radius)
 
     screen.fill(BACKGROUND)
     pygame.display.flip()
     pygame.time.delay(random.randint(200, 800))
+
 
 pygame.quit()
